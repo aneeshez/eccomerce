f5646fe2db78a160558804eab89b2ef772942198
aneeshez <aneeshkumarv2003@gmail.com>
2026-09-25 09:36:22 +0530

    cart module created

--- core/settings.py
+++ core/settings.py
@@ -76,6 +76,11 @@ REST_FRAMEWORK = {
     ),
 }
 
+REST_FRAMEWORK.update({
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+})
+
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
